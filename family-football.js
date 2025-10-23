2cf8b9a277bd1133accbd958efd84aa5aa6c0904
ikari4 <michael.ragusa@me.com>
2025-10-23 15:48:27 -0400

    Fixed typo

--- family-football.js
+++ family-football.js
@@ -1,6 +1,6 @@
 // javascript
 
-fetch('https://api.the-odds-api.com/v4/sports/americalfootball/nfl/odds/?apiKey=1609fd4bd3426401d97740caa596640d&regions=us&markets=spreads&oddsFormat=american')
+fetch('https://api.the-odds-api.com/v4/sports/americanfootball/nfl/odds/?apiKey=1609fd4bd3426401d97740caa596640d&regions=us&markets=spreads&oddsFormat=american')
     .then(function(response) {
         console.log(response.json());
     });
