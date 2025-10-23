5f89890aff55fb86798f28bfecae72e36d9d1455
ikari4 <michael.ragusa@me.com>
2025-10-23 16:02:43 -0400

    Sample URL

--- family-football.js
+++ family-football.js
@@ -1,6 +1,6 @@
 // javascript
 
-fetch('https://api.the-odds-api.com/v4/sports/americanfootball/nfl/odds/?apiKey=1609fd4bd3426401d97740caa596640d&regions=us&markets=spreads&oddsFormat=american')
+fetch('https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey=1609fd4bd3426401d97740caa596640d&regions=us&markets=h2h,spreads&oddsFormat=american')
     .then(function(response) {
         console.log(response.json());
     });
