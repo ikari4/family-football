571c1259a3d583ecb11515cf4bf0a9f48115000b
ikari4 <michael.ragusa@me.com>
2025-10-25 11:26:40 -0400

    saved api key in vercel

--- api/football.js
+++ api/football.js
@@ -1,33 +1,33 @@
 // api/football.js
 
-// export default async function handler(req, res) {
-//   const urlBase = 'https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/';
-//   const apiKey = '1609fd4bd3426401d97740caa596640d';
-//   const regions = 'us';
-//   const markets = 'spreads';
-//   const bookmakers = 'draftkings';
-//   const oddsFormat = 'american';
-//   const url = urlBase +
-//     "?apiKey=" + apiKey + 
-//     "&regions=" + regions + 
-//     "&bookmakers=" + bookmakers + 
-//     "&markets=" + markets + 
-//     "&oddsFormat=" + oddsFormat;
+export default async function handler(req, res) {
+  const urlBase = 'https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/';
+  const apiKey = process.env.API_KEY;
+  const regions = 'us';
+  const markets = 'spreads';
+  const bookmakers = 'draftkings';
+  const oddsFormat = 'american';
+  const url = urlBase +
+    "?apiKey=" + apiKey + 
+    "&regions=" + regions + 
+    "&bookmakers=" + bookmakers + 
+    "&markets=" + markets + 
+    "&oddsFormat=" + oddsFormat;
 
-//   try {
-//     const response = await fetch(url);
+  try {
+    const response = await fetch(url);
 
-//     if (!response.ok) {
-//       return res.status(response.status).json({ error: "API request failed" });
-//     }
+    if (!response.ok) {
+      return res.status(response.status).json({ error: "API request failed" });
+    }
 
-//     const data = await response.json();
+    const data = await response.json();
 
-//     // Return JSON to frontend
-//     res.status(200).json(data);
+    // Return JSON to frontend
+    res.status(200).json(data);
 
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Server Error" });
-//   }
-// }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+}
